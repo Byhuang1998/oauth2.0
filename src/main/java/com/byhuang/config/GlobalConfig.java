c950611b2e71b60bcfb3e8a2abb993f9c3fbd349
byhuang <byhuang1998@gmail.com>
2023-06-18 19:08:09 +0800

    master add JdbcTemplate Bean

---
+++ src/main/java/com/byhuang/config/GlobalConfig.java
@@ -0,0 +1,28 @@
+package com.byhuang.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @date 2023/6/18 18:35
+ * @description TODO
+ */
+@Configuration
+public class GlobalConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource);
+        return jdbcTemplate;
+    }
+}
---
+++ src/test/java/com/byhuang/DatabaseTest.java
@@ -0,0 +1,43 @@
+package com.byhuang;
+
+import com.byhuang.session.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @date 2023/6/18 18:37
+ * @description 数据库操作测试类
+ */
+@SpringBootTest
+public class DatabaseTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void testInsert() {
+        String sql = "insert into user values(?, ?);";
+        jdbcTemplate.update(sql, "tom", "123");
+    }
+
+    @Test
+    public void testSelectAll() {
+        String sql = "select * from user;";
+        List<User> userList = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
+        System.out.println(userList);
+    }
+
+    @Test
+    public void testSelectOne() {
+        String sql = "select * from user where username = ?;";
+        User user = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(User.class), "张三");
+        System.out.println(user);
+    }
+}
