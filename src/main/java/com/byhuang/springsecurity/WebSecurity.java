5d519a5bcfadac037eaf5e71efe0902bcb89446e
byhuang <byhuang1998@gmail.com>
2023-06-25 23:57:59 +0800

    master add annotation and parent class

--- src/main/java/com/byhuang/springsecurity/WebSecurity.java
+++ src/main/java/com/byhuang/springsecurity/WebSecurity.java
@@ -1,11 +1,17 @@
 package com.byhuang.springsecurity;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 /**
  * @author huangbingyi
  * @version 1.0
  * @date 2023/6/21 0:33
  * @description Web安全配置器
  */
-public class WebSecurity {
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
 
 }
