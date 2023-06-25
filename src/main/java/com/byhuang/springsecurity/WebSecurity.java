2c3f3cbd21d4450c43d5381062ec4ea3534c7d76
byhuang <byhuang1998@gmail.com>
2023-06-25 23:53:36 +0800

    master add annotate

--- src/main/java/com/byhuang/springsecurity/WebSecurity.java
+++ src/main/java/com/byhuang/springsecurity/WebSecurity.java
@@ -4,7 +4,7 @@ package com.byhuang.springsecurity;
  * @author huangbingyi
  * @version 1.0
  * @date 2023/6/21 0:33
- * @description TODO
+ * @description Web安全配置器
  */
 public class WebSecurity {
 
