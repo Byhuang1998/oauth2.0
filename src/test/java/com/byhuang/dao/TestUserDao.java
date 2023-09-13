44752f8ff45e628c4c1475ffc515a6b00861e4f2
byhuang1998 <byhuang1998@gmail.com>
2023-09-13 23:14:40 +0800

    master 添加BCrypt密码编码器

--- src/test/java/com/byhuang/dao/TestUserDao.java
+++ src/test/java/com/byhuang/dao/TestUserDao.java
@@ -4,6 +4,7 @@ import com.byhuang.pojo.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 public class TestUserDao {
@@ -11,10 +12,21 @@ public class TestUserDao {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Test
     public void testSelectUserByName() {
         User by = userDao.selectUserByName("by");
         System.out.println(by.toString());
+    }
+
+    @Test
+    public void testBCryptPasswordEncoder() {
+        String encode = bCryptPasswordEncoder.encode("123456");
+        System.out.println("encode: " + encode);
+        boolean matches = bCryptPasswordEncoder.matches("123456", encode);
+        System.out.println(matches);
     }
 
 }
