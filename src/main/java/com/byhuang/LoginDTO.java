70d3df63df0dd56c5bdd0607f9357812eb6faa9a
byhuang1998 <byhuang1998@gmail.com>
2023-09-16 18:08:38 +0800

    master 添加无参构造方法

--- src/main/java/com/byhuang/LoginDTO.java
+++ src/main/java/com/byhuang/LoginDTO.java
@@ -3,12 +3,14 @@ package com.byhuang;
 import com.byhuang.pojo.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class LoginDTO implements UserDetails {
 
